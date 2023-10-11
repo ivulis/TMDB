fb01787bf11ecc2d7335796fdd46aa8b0efa4806
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-10-11 11:28:44 +0300

    Added 'Search movies' fragment's UI and logic

--- app/src/main/java/me/ivulis/jazeps/tmdb/MainActivity.kt
+++ app/src/main/java/me/ivulis/jazeps/tmdb/MainActivity.kt
@@ -1,11 +1,15 @@
 package me.ivulis.jazeps.tmdb
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import me.ivulis.jazeps.tmdb.ui.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +21,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupNavController()
+    }
+
+    private fun setupNavController() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        setupActionBarWithNavController(this, navController)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.movieListFragment, R.id.movieSearchFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.movieDetailFragment) {
+                bottomNavigationView.visibility = View.GONE
+            } else {
+                bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
